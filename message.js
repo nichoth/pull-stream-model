7f3247e8f51ab45c13949dc502f9347ca9cbd85b
Nick Thomas <nichoth@gmail.com>
2017-04-02 17:06:10 -0700

    better msg.find

--- message.js
+++ message.js
@@ -8,10 +8,9 @@ function Message (type, data) {
 Message.type = function (msg) { return msg[0] }
 Message.data = function (msg) { return msg[1] }
 Message.find = function (path, msg) {
-    var _path = path.slice()
     var _msg = msg
-    while (_path.length) {
-        if (_path.shift() === _msg[0]) _msg = msg[1]
+    for (var i = 0; i < path.length; i++) {
+        if (path[i] === _msg[0]) _msg = msg[1]
         else return undefined
     }
     return _msg
